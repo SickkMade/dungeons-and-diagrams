1b59d0f30c97a5e075e79f00b673210f3208c26d
sickk <jaakmyr@gmail.com>
2025-01-16 12:01:44 -0800

    added removing and adding tiles and isdeleting utility

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import Gameboard from "./components/Gameboard"
-import { createContext, useState } from "react"
+import { createContext, useState, useEffect } from "react"
 export const AppContext = createContext();
 
 function App() {
   const [gameBoard, setGameBoard] = useState(Array.from(Array(8), () => new Array(8).fill(0)));
+  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
   const appContextValue = {
     gameBoard,
     setGameBoard,
+    isMouseDown,
+    isDeleting,
+    setIsDeleting,
   }
+
+  useEffect(() => {
+    const mouseDown = () => setIsMouseDown(true);
+    const mouseUp = () => setIsMouseDown(false);
+    window.addEventListener("mousedown", mouseDown);
+    window.addEventListener("mouseup", mouseUp);
+
+    return () => {
+      window.removeEventListener('mousedown', mouseDown);
+      window.removeEventListener('mouseup', mouseUp);
+    };
+  }, [])
 
   return (
     <AppContext.Provider value={appContextValue}>
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,9 +1,51 @@
 import '../css/Tile.css'
-import { useState } from 'react'
+import { useState, useEffect, useRef, useContext} from 'react'
+import { AppContext } from '../App';
 
-function Tile({key}) {
+function Tile() {
+    const [usable, setUsable] = useState(true);
+    const [hasBlock, setHasBlock] = useState(false);
+    const tileRef = useRef(null);
+    const {isMouseDown, setIsDeleting, isDeleting} = useContext(AppContext);
+
+    // this useeffect is for setting blocks
+    useEffect(() => {
+        if(!usable) return;
+
+        const createBlock = () => {
+            if(isMouseDown){
+                if(isDeleting){
+                    setHasBlock(false)
+                } else{
+                    setHasBlock(true)
+                }
+            }
+        }
+
+        const handleClick = () => {
+            
+            if(hasBlock){
+                //if we click on block we are deleting
+                setIsDeleting(true);
+            } else{
+                setIsDeleting(false);
+            }
+
+            createBlock()
+        }
+
+        tileRef.current.addEventListener("mouseover", createBlock);
+        tileRef.current.addEventListener("mousedown", handleClick);
+
+        return () => {
+            tileRef.current.removeEventListener("mousedown", handleClick);
+            tileRef.current.removeEventListener("mouseover", createBlock);
+        }
+    }, [usable, hasBlock, isMouseDown, isDeleting])
+
+    
   return (
-    <div className="tile" key={key}></div>
+    <div ref={tileRef} className={`tile ${hasBlock ? 'block' : ''}`}></div>
   )
 }
 
