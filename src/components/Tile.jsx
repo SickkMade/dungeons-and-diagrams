ff52ef9b314c1c6a6e0e6a9a7c7b4b1bb2934f00
sickk <jaakmyr@gmail.com>
2025-01-16 12:08:13 -0800

    fixed click issues making tiles late

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -11,35 +11,41 @@ function Tile() {
     // this useeffect is for setting blocks
     useEffect(() => {
         if(!usable) return;
+        const createBlock = (isDeleting) => {
+            if(isDeleting){
+                setHasBlock(false)
+            } else{
+                setHasBlock(true)
+            }
+        }
 
-        const createBlock = () => {
+        //seperated out because handle click state doesnt change isMouseDown fast enough
+        const handleMouseDown = () => {
             if(isMouseDown){
-                if(isDeleting){
-                    setHasBlock(false)
-                } else{
-                    setHasBlock(true)
-                }
+                createBlock(isDeleting)
             }
         }
 
         const handleClick = () => {
             
+            //i need to fix these state issues, these are silly workarounds
             if(hasBlock){
                 //if we click on block we are deleting
                 setIsDeleting(true);
+                createBlock(true);
+
             } else{
+                createBlock(false);
                 setIsDeleting(false);
             }
-
-            createBlock()
         }
 
-        tileRef.current.addEventListener("mouseover", createBlock);
+        tileRef.current.addEventListener("mouseover", handleMouseDown);
         tileRef.current.addEventListener("mousedown", handleClick);
 
         return () => {
             tileRef.current.removeEventListener("mousedown", handleClick);
-            tileRef.current.removeEventListener("mouseover", createBlock);
+            tileRef.current.removeEventListener("mouseover", handleMouseDown);
         }
     }, [usable, hasBlock, isMouseDown, isDeleting])
 
